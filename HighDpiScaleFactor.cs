6c831bb840eec97f971f10941f175d88b11abb3f
Adrian Akison <adrian@akison.com>
2018-05-05 17:33:45 +1000

    Remove unused using, expose public scale options

--- HighDpiScaleFactor.cs
+++ HighDpiScaleFactor.cs
@@ -6,12 +6,18 @@ using UnityEngine.UI;
 [RequireComponent(typeof(CanvasScaler))]
 public class HighDpiScaleFactor : MonoBehaviour {
 
+    [Tooltip("The DPI to switch to 2X mode, recommended value is 250.")]
+    public float doubleDpi = 250;
+
+    [Tooltip("The DPI to switch to 3X mode, recommended value is 360.")]
+    public float tripleDpi = 360;
+
     internal void Start () {
         var scaler = GetComponent<CanvasScaler>();
-        if(Screen.dpi >= 360 || Screen.height > 2100) { // iPad and iPhone screenshots on PC.
+        if(Screen.dpi >= tripleDpi || Screen.height > 2100) { // iPad and iPhone screenshots on PC.
             scaler.scaleFactor = 3;
         }
-        else if(Screen.dpi > 250 || Screen.height > 1050) {
+        else if(Screen.dpi > doubleDpi || Screen.height > 1050) {
             scaler.scaleFactor = 2;
         }
         else {
