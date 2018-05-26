7c9c84417949df0e53e1e76acc61e1189cd7767e
Adrian Akison <adrian@akison.com>
2018-05-26 14:01:10 +1000

    Add DialogControl to available controls

---
+++ DialogControl.cs
@@ -0,0 +1,129 @@
+﻿using DG.Tweening;
+using Salsa;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class DialogControl : MonoBehaviour {
+
+    [Tooltip("The amount of time it takes to transition from one state to another.")]
+    public float duration = 0.5f;
+
+    public StartPosition startPosition = StartPosition.Hidden;
+
+    private StartPosition position;
+
+    [Header("Show Transform (use menu)")]
+    public Vector3 showPosition;
+    public Vector3 showRotation;
+    public Vector3 showScale;
+
+    [Header("Hide Transform (use menu)")]
+    public Vector3 hidePosition;
+    public Vector3 hideRotation;
+    public Vector3 hideScale;
+
+    [ContextMenu("Set Show Transform")]
+    public void SetShowTransform() {
+        showPosition = transform.position;
+        showRotation = transform.rotation.eulerAngles;
+        showScale = transform.localScale;
+    }
+
+    [ContextMenu("Move to Show Transform")]
+    public void MoveToShowTransform() {
+        transform.position = showPosition;
+        transform.rotation = Quaternion.Euler(showRotation);
+        transform.localScale = showScale;
+        position = StartPosition.Shown;
+    }
+
+    [ContextMenu("Set Hide Transform")]
+    public void SetHideTransform() {
+        hidePosition = transform.position;
+        hideRotation = transform.rotation.eulerAngles;
+        hideScale = transform.localScale;
+    }
+
+    [ContextMenu("Move to Hide Transform")]
+    public void MoveToHideTransform() {
+        transform.position = hidePosition;
+        transform.rotation = Quaternion.Euler(hideRotation);
+        transform.localScale = hideScale;
+        position = StartPosition.Hidden;
+    }
+
+    private void Start() {
+        transform.position = startPosition == StartPosition.Hidden ? hidePosition : showPosition;
+        position = startPosition;
+    }
+
+    public void Show() {
+        if(position == StartPosition.Hidden) {
+            StartCoroutine(ShowCo());
+        }
+    }
+
+    private IEnumerator ShowCo() {
+        position = StartPosition.Moving;
+        ShowHideChildren(true);
+        transform.DOMove(showPosition, duration, true);
+        transform.DORotate(showRotation, duration);
+        transform.DOScale(showScale, duration);
+        yield return WaitFor.Seconds(duration);
+        position = StartPosition.Shown;
+    }
+
+    public void Hide() {
+        if(position == StartPosition.Shown) {
+            StartCoroutine(HideCo());
+        }
+    }
+
+    private IEnumerator HideCo() {
+        position = StartPosition.Moving;
+        transform.DOMove(hidePosition, duration, true);
+        transform.DORotate(hideRotation, duration);
+        transform.DOScale(hideScale, duration);
+        yield return WaitFor.Seconds(duration);
+        ShowHideChildren(false);
+        position = StartPosition.Hidden;
+    }
+
+    public void Toggle() {
+        if(position == StartPosition.Shown) {
+            Hide();
+        }
+        else if(position == StartPosition.Hidden) {
+            Show();
+        }
+    }
+
+    public void Peek(float seconds) {
+        hideTime = Time.time + seconds;
+        StartCoroutine(PeekRoutine(seconds));
+    }
+
+    private IEnumerator PeekRoutine(float seconds) {
+        Show();
+        while(Time.time < hideTime) {
+            yield return WaitFor.EndOfFrame;
+        }
+        Hide();
+    }
+    private float hideTime = float.MaxValue;
+
+    private void ShowHideChildren(bool active) {
+        for(int i = 0; i < transform.childCount; ++i) {
+            transform.GetChild(i).gameObject.SetActive(active);
+        }
+    }
+
+}
+
+public enum StartPosition {
+    Shown,
+    Moving,
+    Hidden,
+}
--- README.md
+++ README.md
@@ -14,3 +14,13 @@ There are no demo scenes or resources in this repo, just the required files so t
 
 Add this component to the root canvas (that has the Canvas Scalar component), it will detect the screen resolution and size and switch scale factor between 1x, 2x, and 3x.
 
+## Feature: Dialog Control
+
+Add this component to a UI object that is designed to hold a dialog box.  To use:
+* Add the `DialogControl` to the root of the dialog (typically within Canvas)
+* Set the transform (position, rotation and scale) to the shown position.
+* Click on the context menu of the `DialogControl` in the inspector, then "Set Show Transform"
+* Move the transform to the hidden location, typically offscreen.
+* Click on the context menu of the `DialogControl` in the inspector, then "Set Hide Transform"
+* In the button event of UI controls, call `Show()`, `Hide()` or `Toggle()` to trigger animation between states.
+* Settings allow configuration animation and initial position.
